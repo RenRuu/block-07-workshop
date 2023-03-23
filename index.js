0bb5564c99898dd5ad450c765cf0e6c556bbf53f
Rin <126840436+RenRuu@users.noreply.github.com>
2023-03-23 15:51:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// User is greeted by alert containing the combination of the vault.
+// Pressing okay will clear this alert and direct user to main page.
+// Main page will display a secret message to the meaning of life.
+
+let comb1 = 24 + 4 - 1; // =27
+let comb2 = 50 + 24; // =74
+let comb3 = 74 + 4 - 1; // =77
+
+
+alert("Quickly input this code" + " " + comb1 + "-" + comb2 + "-" + comb3)
